a48873cab7b98efaac171945fa412a85020cc560
Adithya Nair <git@adtya.xyz>
2021-05-15 18:53:43 +0530

    Update deploy configs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from './components/Nav'
+import NoMatch from "./NoMatch";
+
 function App() {
-    return null;
+    return (
+        <Router>
+            <Nav/>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/about">
+                    <About/>
+                </Route>
+                <Route path="*">
+                    <NoMatch/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
