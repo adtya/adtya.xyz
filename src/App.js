ed5732eb80779e5580d9eb348faf3e4ca0ebdd50
Adithya Nair <git@adtya.xyz>
2021-05-15 18:58:15 +0530

    Revert to placeholder file for now

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-
-import About from "./components/About";
-import Home from "./components/Home";
-import Nav from './components/Nav'
-import NoMatch from "./NoMatch";
-
 function App() {
-    return (
-        <Router>
-            <Nav/>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route exact path="/about">
-                    <About/>
-                </Route>
-                <Route path="*">
-                    <NoMatch/>
-                </Route>
-            </Switch>
-        </Router>
-    );
+    return null;
 }
 
 export default App;
