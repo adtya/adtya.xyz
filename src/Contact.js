b3e7234192ede58cb93b68ddff185d513e25b786
Adithya Nair <git@adtya.xyz>
2021-05-22 18:40:45 +0530

    open external links in new tab and others in same tab

--- src/Contact.js
+++ src/Contact.js
@@ -3,11 +3,11 @@ function Contact() {
         <main>
             <h1>Contact Me</h1>
             <p>You can get in touch with me over <a href={"mailto:hi@adtya.xyz"}>email</a> or on Mastodon at <a
-                href={"https://fosstodon.org/@adtya"}>Fosstodon</a> or <a href={"https://aana.site/@adtya"}>ആന
-                സൈറ്റ്</a>.
+                href={"https://fosstodon.org/@adtya"}>Fosstodon</a> or <a
+                href={"https://aana.site/@adtya"}>ആന.സൈറ്റ്</a>.
                 <br/>
                 If you're hearing the word Mastodon for the first time, you can check it out <a
-                    href={"https://joinmastodon.org/"}>here</a>.
+                    target={"_blank"} rel={"noreferrer"} href={"https://joinmastodon.org/"}>here</a>.
             </p>
         </main>
     );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,16 +14,16 @@ function Footer() {
                    href={"https://keys.openpgp.org/vks/v1/by-fingerprint/95EA0E6BF507E8EA346AC8740C7F35F4F821290F"}>
                     {OpenPGP}
                 </a>
-                <a className={"socialIcon"} href={"https://fosstodon.org/@adtya"} target={"_blank"} rel={"noreferrer"}>
+                <a className={"socialIcon"} href={"https://fosstodon.org/@adtya"}>
                     {Mastodon}
                 </a>
-                <a className={"socialIcon"} href={"https://aana.site/@adtya"} target={"_blank"} rel={"noreferrer"}>
+                <a className={"socialIcon"} href={"https://aana.site/@adtya"}>
                     {Mastodon}
                 </a>
-                <a className={"socialIcon"} href={"https://pixelfed.social/adtya"} target={"_blank"} rel={"noreferrer"}>
+                <a className={"socialIcon"} href={"https://pixelfed.social/adtya"}>
                     {Instagram}
                 </a>
-                <a className={"socialIcon"} href={"https://github.com/adtya"} target={"_blank"} rel={"noreferrer"}>
+                <a className={"socialIcon"} href={"https://github.com/adtya"}>
                     {Github}
                 </a>
             </div>
