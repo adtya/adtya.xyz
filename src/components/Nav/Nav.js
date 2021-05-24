8b67d1f4f1f88e5c7c493b3aa733bb4d638c7446
Adithya Nair <git@adtya.xyz>
2021-05-24 15:19:14 +0530

    active class for nav buttons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
-import './nav.module.css';
+import styles from './nav.module.css';
 
 class Nav extends React.Component {
     render() {
         return (
             <nav>
-                <NavLink exact activeClassName="this" to="/">Home</NavLink>
-                <NavLink exact activeClassName="this" to="/about">About Me</NavLink>
-                <NavLink exact activeClassName="this" to="/contact">Contact Me</NavLink>
+                <NavLink exact activeClassName={styles.this} to="/">Home</NavLink>
+                <NavLink exact activeClassName={styles.this} to="/about">About Me</NavLink>
+                <NavLink exact activeClassName={styles.this} to="/contact">Contact Me</NavLink>
             </nav>
         );
     }
