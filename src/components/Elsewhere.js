10ffaf4aaa102b1d2d9575168ac77598f2a025ee
Adithya Nair <git@adtya.xyz>
2021-05-27 00:58:17 +0530

    add some elsewhere

--- src/components/Elsewhere.js
+++ src/components/Elsewhere.js
@@ -2,7 +2,30 @@ import React from "react";
 
 class Elsewhere extends React.Component {
     render() {
-        return null;
+        return (
+            <main>
+                <h3>Other places on the interwebs one might find me ...</h3>
+                <ul>
+                    <li>
+                        <a href={"https://drunkphilosophy.live"} target={"_blank"} rel={"noreferrer"}>
+                            Drunk Philosophy
+                        </a>
+                        <small>
+                            , a place ran by me and some friends. A place of which the sole purpose is to make no sense.
+                        </small>
+                    </li>
+                    <li>
+                        <a href={"https://knitlabs.org"} target={"_blank"} rel={"noreferrer"}>
+                            Knitlabs
+                        </a>
+                        <small>
+                            , a place to put random pieces of code that we make, most, like all side projects born
+                            one evening and die the next morning.
+                        </small>
+                    </li>
+                </ul>
+            </main>
+        );
     }
 }
 
