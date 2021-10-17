80f660a705ad9a8b2f76ed1e6ee0e6b65c26dcdc
Adithya Nair <adtya@adtya.xyz>
2021-10-17 22:02:58 +0530

    Add Wiki link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,17 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import styles from './nav.module.css';
 
 class Nav extends React.Component {
     render() {
         return (
-            <nav style={{align: 'center'}}>
+            <nav style={{ align: 'center' }}>
                 <NavLink exact activeClassName={styles.this} to="/">About Me</NavLink>
                 <NavLink exact activeClassName={styles.this} to="/contact">Contact</NavLink>
                 <NavLink exact activeClassName={styles.this} to="/elsewhere">Elsewhere</NavLink>
                 <a href={"https://blog.adtya.xyz"}>Blog</a>
+                <a href={"https://wiki.adtya.xyz"}>Wiki</a>
             </nav>
         );
     }
